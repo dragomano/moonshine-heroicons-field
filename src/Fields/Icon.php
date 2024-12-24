811d8401e2bc069d868fe4108924c14bbc5f3f76
Bugo <cheetah@bk.ru>
2024-12-25 00:21:12 +0500

    Refactor code

--- src/Fields/Icon.php
+++ src/Fields/Icon.php
@@ -21,7 +21,7 @@ class Icon extends Select
 
         $this->currentStyle = $this->getStyle();
         $this->options = $this->getCustomOptions();
-        $this->optionProperties = fn() => $this->getCustomOptionProperties();
+        $this->optionProperties = $this->getCustomOptionProperties();
     }
 
     public function options(Closure|array $data): static
@@ -45,8 +45,8 @@ class Icon extends Select
     {
         return match ($style) {
             'c', 'micro'   => 'c',
-            'o', 'outline' => 'o',
             'm', 'mini'    => 'm',
+            'o', 'outline' => 'o',
             default        => 's'
         };
     }
@@ -71,7 +71,7 @@ class Icon extends Select
         $icons = array_filter(explode(',', $value));
 
         $result = array_map(
-            fn($icon) => svg("heroicon-$this->currentStyle-$icon", 'h-6 w-6')->toHtml(),
+            static fn($icon) => svg("heroicon-$this->currentStyle-$icon", 'h-6 w-6')->toHtml(),
             $icons
         );
 
@@ -81,9 +81,9 @@ class Icon extends Select
 
     private function getCustomOptions(): array
     {
-        return Cache::rememberForever("heroicons-$this->currentStyle-field-options", function () {
+        return Cache::rememberForever("heroicons_{$this->currentStyle}_field_options", function () {
             $items = glob(public_path("vendor/blade-heroicons/$this->currentStyle-*.svg"));
-            $items = array_map(fn($item) => substr(basename($item, '.svg'), 2), $items);
+            $items = array_map(static fn($item) => substr(basename($item, '.svg'), 2), $items);
 
             return array_combine($items, $items);
         });
@@ -91,10 +91,10 @@ class Icon extends Select
 
     private function getCustomOptionProperties(): array
     {
-        return Cache::rememberForever("heroicons-$this->currentStyle-field-option-properties", function () {
+        return Cache::rememberForever("heroicons_{$this->currentStyle}_field_option_properties", function () {
             $link = asset("vendor/blade-heroicons/$this->currentStyle-%s.svg");
 
-            return array_map(fn($item) => ['image' => sprintf($link, $item)], $this->getCustomOptions());
+            return array_map(static fn($item) => ['image' => sprintf($link, $item)], $this->getCustomOptions());
         });
     }
 }
